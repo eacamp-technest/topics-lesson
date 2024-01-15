391a02cb399ef5c732eadfd9fee91f2bd44c3904
khayym <xeyyam.kerimov@gmail.com>
2024-01-15 21:17:10 +0400

    asdad

---
+++ index.js
@@ -0,0 +1,30 @@
+// const name = "xeyyam"
+// const lastName = 'karimov'
+// const email = 'xeyyam.kerimov@gmail.com';
+// const password = 'asdiuahsiduh12312'
+// const age = 24;
+// const isAgree = false;
+// const phoneNumber = null;
+
+const user = {
+    name: 'xeyyam',
+    surname: 'kkarasda',
+    passInfo: {
+        id: 234234,
+        faxNumber: 213423423,
+        name: 'Xarici passport',
+        packs: {
+            serial: 234723,
+            secretNumber: 234234,
+            secretName: 'ashdaid aiusdhai aisudaid as'
+        }
+    }
+}
+
+const ownKey = 'passInfo'
+console.log(ownKey[1])
+
+// console.log(user['passInfo']['packs']['secretName']);
+
+
+
