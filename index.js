c488d4963f3d57c162dfc84c79b982a2d368ac9e
khayym <xeyyam.kerimov@gmail.com>
2024-01-17 21:17:53 +0400

    arrays, obj

--- index.js
+++ index.js
@@ -6,25 +6,69 @@
 // const isAgree = false;
 // const phoneNumber = null;
 
-const user = {
-    name: 'xeyyam',
-    surname: 'kkarasda',
-    passInfo: {
-        id: 234234,
-        faxNumber: 213423423,
-        name: 'Xarici passport',
-        packs: {
-            serial: 234723,
-            secretNumber: 234234,
-            secretName: 'ashdaid aiusdhai aisudaid as'
-        }
-    }
+// test = '1212'
+
+
+// const user = {
+//     name: 'xeyyam',
+//     surname: 'karimov',
+//     info: {
+//         id: 12,
+//         phone: '055xxxxxxx',
+//         taxN: '23428',
+//     }
+// }
+
+// const { name, ...rest } = user
+// console.log(rest);
+// const name = user
+// const username = user.name
+// const insideName = user.info.sec.i.ad
+
+
+// user['age'] = 12
+// user.phone = 83
+
+// delete user.name
+// delete user.phone
+
+
+
+
+
+// const user = {
+//     'name': 'Alex',
+//     'address': '15th Park Avenue',
+//     'age': 43
+// }
+
+// const car = {
+//     name: 'volvo',
+//     id: 12,
+//     banId: '123',
+//     color: 'red'
+// }
+
+
+// const post = {
+//     auth: 'wastelog',
+//     data: 'www.com.inst/.com',
+//     like: 123,
+//     desc: "Sadriye Teyze de Waste Loglu! O lezzetli kızartmalardan sonra bitkisel atık yağlarını biriktiriyor"
+// }
+
+
+
+
+const car = {
+    name: 'bmw',
+    aisud: 'adsoasd',
+    sjsje: '2123123'
 }
 
-const ownKey = 'passInfo'
-console.log(ownKey[1])
 
-// console.log(user['passInfo']['packs']['secretName']);
+const arr = [car, car, car, car, car];
 
-
+const xx = arr[1]
+console.log(xx.name);
 
