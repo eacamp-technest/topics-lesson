78deef120b414c60891d7aa5ee5038876d82f93d
khayym <xeyyam.kerimov@gmail.com>
2024-01-22 22:35:19 +0400

    if else conditions

---
+++ ifelse.js
@@ -0,0 +1,77 @@
+const a = false;
+
+const y = 4;
+
+
+// if (y - x !== 2 && !a || x + y === 6) {
+//     console.log('is true')
+// } else {
+//     console.log(' is false')
+// }
+
+// const x = 2;
+
+
+// if (x === 2) {
+//     console.log('x === 2');
+//     return x
+// } else if (x === 3) {
+//     console.log('x === 3');
+// } else if (x === 4) {
+//     console.log('x === 4');
+// } else {
+//     console.log('x !== any');
+// }
+
+// true = 1
+// false = 0
+
+let email;
+
+email = !email ? 'xeyyam.kerimov@gmail.com' : email
+
+let age = 22;
+
+age = age < 20 ? 'it is child' : 'it is adult'
+
+if (age < 20) {
+    age = 'it is child';
+} else {
+    age = 'it is adult';
+}
+
+console.log(age);
+
+//  false / null / undefined / NaN / ""
+
+
+// if (!email) {
+//     email = 'xeyyam.kerimov@gmail.com'
+// }
+
+
+
+
+
+
+
+// if (!email) {
+//     email = 'xeyyam.kerimov@gmail.com'
+// }
+
+// console.log('type num:', email);
+
+// if (typeof num === 'string') {
+//     num = 2;
+// }
+
+// if (typeof num === 'boolean') {
+//     num = 2;
+// }
+
+// if (typeof num === 'undefined') {
+//     num = 2;
+// }
+
+
+
