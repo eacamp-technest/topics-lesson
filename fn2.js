2f47b23b44ce2907bcf09a82728eac080fabaedb
khayym <xeyyam.kerimov@gmail.com>
2024-01-29 22:12:17 +0400

    fn2js

---
+++ fn2.js
@@ -0,0 +1,108 @@
+// let fullname = 'Xeyyam Kerimov';
+
+// function showName() {
+//     console.log(fullname);
+//     let newName = 'Azer'
+// }
+
+
+
+// const secKey = 'ajsdhiasdn uashdiahsn asdu agsuyasd';
+
+// function beEnc(x) {
+//     const keys = secKey
+//     return x + keys;
+// }
+
+// function encBefore(a) {
+//     const keys = secKey
+//     return keys + a;
+// }
+
+
+// var age = 24;
+
+// function calc(x, y) {
+
+//     if (x === 0) {
+//         const newX = 10;
+//     }
+
+//     if (y === 0) {
+//         var newY = 20;
+//     }
+
+//     console.log('-> ', x + y);
+
+//     console.log('other: ', newX + newY)
+// }
+
+
+
+
+// // calc(0, 0);
+
+// // console.log(newX, newY)
+// // console.log(newAge)
+
+
+
+
+
+
+// // showName()
+// // console.log(newName)
+
+
+// const x = 'xeyyam';
+
+// // const x = 12;
+
+// let a = 'aisdh'
+
+// // let a = 'asd'
+
+// var name = 'asdsad';
+
+// const asd = 'asd';
+// var az = '213'
+
+
+
+// console.log('with const', cName);
+// console.log('with let', lName);
+// console.log('with var', vName);
+
+// show()
+
+// function show() {
+//     console.log(vName)
+// }
+
+
+
+// if (vName !== undefined) {
+//     console.log('+')
+// } else {
+//     console.log('-')
+// }
+
+// const cName = 'azer';
+// let lName = 'vuqar';
+// var vName = 'gullu';
+
+
+// let numbers = [1, 2, 3, 4, 5];
+// let sum = calculateSum(numbers);
+
+// function calculateSum(numbers) {
+//     for (let i = 0; i < numbers.length; i++) {
+//         sum += numbers[i];
+//     }
+//     return sum;
+// }
+
+
+
+// let result = sum;
+// console.log(result);
---
+++ x.js
@@ -0,0 +1,42 @@
+const dayOfWeek = 'xeyyam';
+
+// switch (dayOfWeek) {
+//     case 'Monday':
+//         console.log('It is monday')
+//         break;
+
+//     case 'Saturday':
+//         console.log('It is saturday');
+//         break;
+
+//     case 'Tuesday':
+//         console.log('it is Tuesday')
+//         break;
+
+//     case 'Wednesday':
+//         console.log('it is Wednesday');
+//         break;
+
+//     case 'Friday':
+//         console.log('it is Friday');
+//         break;
+
+//     default:
+//         console.log('wrong day of week')
+//         break;
+// }
+
+
+const numbers = [13, 44, 12, 67, 34]; // 5
+
+// for (let index = 0; index <= numbers.length; index++) {
+//     console.log(index);
+// }
+
+const fruits = ['Banan', 'Alma', 'Armud', 'Xiyar'];
+
+
+for (let index = 0; index < fruits.length; index++) {
+
+}
+
