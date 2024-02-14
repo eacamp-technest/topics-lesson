283bf3a8ff3069591b3b1f4ae3c737ea1505de0b
khayym <xeyyam.kerimov@gmail.com>
2024-02-14 21:08:22 +0400

    string methods

---
+++ methods/string.js
@@ -0,0 +1,136 @@
+// const fullname = 'xeyyam';
+// const arr = [2, 3, 's', 'ddde']
+
+// console.log(arr[1])
+// console.log(fullname.at(-3))
+// console.log(fullname[fullname.length - 3])
+
+// const sentence = 'Dog';
+
+// // const arr = [1, 2, 4, 5];
+
+// console.log(sentence.charCodeAt(1))
+// console.log(sentence.charAt(1))
+
+const userName = 'Xeyyam';
+const surname = 'Karimov';
+
+
+// const arr = ['baku', 'davaci', 'QAbBla'].forEach(element => {
+//     return element.toLocaleUpperCase()
+// })
+
+// arr.fopEach(city => {
+//     console.log(city.toLocaleUpperCase())
+//     city = city.toLowerCase()
+// })
+
+// console.log(arr)
+
+
+// const concatFactory = ({ name, elements }) => {
+//     const updated = elements.map(element => {
+//         return " " + element
+//     })
+
+//     // return name.concat(' ', ...updated)
+
+//     return {
+//         name,
+//         elements,
+//         coactedName: name.concat(' ', ...updated)
+//     }
+// }
+
+
+// const res = concatFactory({
+//     name: 'Xeyyam',
+//     elements: ['Karimov', 'Baku', 'AZE']
+// })
+
+// console.log(res)
+
+// console.log(userName.concat(surname))
+// console.log(`${userName} ${surname} `)
+
+// console.log(leeter1.concat(' ', str2));
+// // Expected output: "Hello World"
+
+// console.log(str2.concat(', ', str1));
+// Expected output: "World, Hello"
+
+
+// console.log(str1.endsWith('best! ', str1.length - 1))
+// console.log(str1.startsWith('are', 1))
+
+
+// const fun = (name, surname) => {
+
+// }
+
+
+
+
+// console.log(str1.indexOf('c'))
+
+// const indexOfInCaseSens = (letter, searchChar) => {
+//     // console.log('u', searchChar.toUpperCase())
+//     // console.log('l', searchChar.toLowerCase())
+//     console.log(letter.indexOf(searchChar.toUpperCase()))
+//     console.log(letter.indexOf(searchChar.toLowerCase()))
+//     // letter.indexOf(searchChar.toUpperCase()) || letter.indexOf(searchChar.toLowerCase())
+// }
+
+// console.log(test('xeyyam kerimov', 'YY', 'lower'))
+// const str1 = 'CatS aRE tHe best! 1';
+
+// // console.log(str1.indexOf('c'))
+// console.log(indexOfInCaseSens(str1, 'c'));
+
+
+// const arr = ['xeyyam', 'aze', '23', 22, undefined, 'baku']
+
+// console.log(str1.includes('ar'))
+// console.log(arr.includes(undefined));
+
+// const paragraph = "I";
+
+// const time = '22'
+// // console.log(time.padEnd(time.length + 3, ':00'));
+
+// // const newTime = `${time}:00`
+// console.log(time.concat(':', '00'))
+
+
+// const mood = 'Happy! ';
+
+// // console.log(`I feel ${'xeyyam '.repeat(3)}`);
+
+// const fakeCount = '2';
+
+// console.log(fakeCount)
+// console.log(fakeCount.repeat(2))
+// Expected output: "I feel Happy! Happy! Happy! "
+
+
+
+// console.log(str.slice(25));
+// // Expected output: "the lazy dog."
+
+// console.log(str.slice(4, 25));
+// Expected output: "quick brown fox"
+
+// console.log(str.slice(25, 5));
+// Expected output: "dog."
+
+// sconsole.log(str.slice(-9, -5));
+// Expected output: "lazy"
+
+const str = 'cHe quick bRWwB Sdx FuSps over the lazy dog.';
+
+
+const xx = (lefter) =>
+    lefter.charAt(0).toUpperCase() + lefter.slice(1).toLowerCase();
+
+
+console.log(xx(str))
