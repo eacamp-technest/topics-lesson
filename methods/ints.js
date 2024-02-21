ebb13c9cbe88d5deb27f24349955cf0154fde890
khayym <xeyyam.kerimov@gmail.com>
2024-02-21 21:08:55 +0400

    ints

---
+++ methods/ints.js
@@ -0,0 +1,15 @@
+const x = Number.MAX_SAFE_INTEGER * Number.MAX_SAFE_INTEGER;
+const y = '123'
+// isInteger()
+// isSafeInteger()
+
+
+// const z = '11.12%';
+// const zx = '4abcdefgh.2323'
+
+const num = 12.456353537612
+
+// console.log('parseInt: ', parseInt(num))
+// console.log('Number: ', Number(num))
+
+console.log(String(num))
